bb784fec13180c391d33bf4df62ea95f4ff4cd85
PolinaGancheva <50555933+PolinaGancheva@users.noreply.github.com>
2019-11-14 21:36:07 +0200

    Update 02. JSON's Table.js

--- JavaScript Advanced/02. Objects/02. JSON's Table.js
+++ JavaScript Advanced/02. Objects/02. JSON's Table.js
@@ -18,7 +18,3 @@ function htmlEscape(text) {
         "'": '&#39;', '<': '&lt;', '>': '&gt;' };
     return text.replace(/[\"&'<>]/g, ch => map[ch]);
 }
-solve(['{"name":"Pesho","position":"Promenliva","salary":100000}',
-'{"name":"Teo","position":"Lecturer","salary":1000}',
-'{"name":"Georgi","position":"Lecturer","salary":1000}']
-);
